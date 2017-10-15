c1ea3bf4116febe6d92b4de6e47dc9d7ce8b1bf3
YACINE <YACINE@LENOVO>
2017-10-15 18:22:07 +0100

    dataSource update

--- src/technique/DataSource.java
+++ src/technique/DataSource.java
@@ -5,13 +5,42 @@
  */
 package technique;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
- * @author Sami
+ * @author YACINE
  */
 public class DataSource {
-    String url ="jdbc:mysql://localhost:3306/espritforall";
-    String
-            
+
+    String url = "jdbc:mysql://localhost:3306/espritforall";
+    String login = "root";
+    String password = "";
+    private Connection connection;
+
+    private static DataSource instance;
+
+    public DataSource() {
+        try {
+            connection = DriverManager.getConnection(url, login, password);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static DataSource getInstance() {
+        if (instance == null) {
+            instance = new DataSource();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
     
+
 }
