c0acad4ddd7adda94707e2b172e32eb792e31970
firashamila33 <firas.hamila@esprit.tn>
2017-10-18 19:33:44 -0700

    first CRUD & DAO commit

---
+++ src/services/CommandeService.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import interfaces.ICommandeService;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import models.Commande;
+import models.User;
+import technique.DataSource;
+
+/**
+ *
+ * @author plazma33
+ */
+public class CommandeService implements ICommandeService {
+    
+    Connection connection;
+    
+    public CommandeService() {
+        connection = DataSource.getInstance().getConnection();
+
+    }
+    
+    public void add(Commande t) {
+        String req = "insert into Commande (user_id,heure,prix) values (?,?,?)";
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = connection.prepareStatement(req);
+            preparedStatement.setInt(1, t.getUser().getId());
+            preparedStatement.setDate(2, t.getDateajout());
+            preparedStatement.setDouble(3, t.getPrix());
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    
+    public void delete(Integer id) {
+       String req="delete from Commande where id=?";
+       PreparedStatement preparedStatement;
+       try{
+           preparedStatement= connection.prepareStatement(req);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+       }catch(SQLException ex)
+       {
+       ex.printStackTrace();}
+    }
+    
+    public void update(Commande t) {
+        String req = "update commande set user_id=?, heure =?, prix=? where id = ?";
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = connection.prepareStatement(req);
+            preparedStatement.setInt(1, t.getUser().getId());
+            preparedStatement.setDate(2, t.getDateajout());
+            preparedStatement.setDouble(3, t.getPrix());
+            preparedStatement.setDouble(4, t.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    
+    public List<Commande> getAll() {
+        List<Commande> orders = new ArrayList<>();
+        String req = "select * from Commande";
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = connection.prepareStatement(req);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Commande c = new Commande(resultSet.getInt("id"), new User(resultSet.getInt("user_id")), resultSet.getDate("heure"),resultSet.getDouble("prix"));
+                System.out.println(c);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return orders;
+    }
+    
+    
+    
+    public Commande getById(Integer r) {
+        Commande commande = null;
+        String req = "select * from Commande where id=?";
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = connection.prepareStatement(req);
+            preparedStatement.setInt(1, r);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                 commande = new Commande(resultSet.getInt("id"), new User(resultSet.getInt("user_id")), resultSet.getDate("heure"),resultSet.getDouble("prix"));
+                 }
+         } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return commande;
+    }
+    
+
+
+    public Commande getByUserId(Integer r) {
+        Commande commande = null;
+        String req = "select * from Commande where user_id=?";
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = connection.prepareStatement(req);
+            preparedStatement.setInt(1, r);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                 commande = new Commande(resultSet.getInt("id"), new User(resultSet.getInt("user_id")), resultSet.getDate("heure"),resultSet.getDouble("prix"));
+                 }
+         } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return commande;
+    }
+    
+    @Override
+    public Commande search(Commande t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
