e528bf6d21c28f3cd600dee2925fcb7bbdd3e251
kadhem <okadhem@gmail.com>
2017-10-19 00:01:23 +0000

    implement add for AnnonceCoLocationService

--- AnnonceScript.sql
+++ AnnonceScript.sql
@@ -1,2 +1,14 @@
 CREATE TABLE `espritforall`.`AnnonceObjetPerdu` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(50) NOT NULL , `description` VARCHAR(50) NOT NULL , `owner_id` INT NOT NULL , `creationDate` DATE NOT NULL , `expirationDate` DATE NOT NULL , `objectDescription` VARCHAR(50) NOT NULL , `lossDate` DATE NOT NULL , `lossLocation` VARCHAR(100) NOT NULL , PRIMARY KEY (`id`));
+
 ALTER TABLE `AnnonceObjetPerdu` ADD INDEX(`owner_id`);
+
+CREATE TABLE `espritforall`.`AnnonceCoLocation` ( `id` INT NOT NULL AUTO_INCREMENT , `address_id` INT NOT NULL , `dimensions` VARCHAR(50) NOT NULL , `maxCoLocataire` INT NOT NULL , `loyer` FLOAT NOT NULL , `name` VARCHAR(50) NOT NULL , `description` VARCHAR(50) NOT NULL , `owner_id` INT NOT NULL , `creationDate` DATE NOT NULL , `expirationDate` DATE NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+ALTER TABLE `AnnonceCoLocation` ADD INDEX(`address_id`);
+ALTER TABLE `AnnonceCoLocation` ADD INDEX(`owner_id`);
+ALTER TABLE `AnnonceCoLocation` ADD CONSTRAINT `fk_ownerId` FOREIGN KEY (`owner_id`) REFERENCES `User`(`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE `AnnonceCoLocation` ADD CONSTRAINT `fk_addressId` FOREIGN KEY (`address_id`) REFERENCES `Address`(`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+CREATE TABLE `espritforall`.`ImageAnnonce` ( `id` INT NOT NULL AUTO_INCREMENT , `annonceCoLocation_id` INT NOT NULL , `imageUrl` VARCHAR(100) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `espritforall`.`AnnonceCoLocataire_R` ( `id` INT NOT NULL AUTO_INCREMENT , `annonceCoLocation_id` INT NOT NULL , `User_id` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+ALTER TABLE `AnnonceCoLocataire_R` ADD CONSTRAINT `fk_AnnonceCoLocationId` FOREIGN KEY (`annonceCoLocation_id`) REFERENCES `AnnonceCoLocation`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `AnnonceCoLocataire_R` ADD CONSTRAINT `fk_UserId` FOREIGN KEY (`User_id`) REFERENCES `User`(`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE `ImageAnnonce` ADD CONSTRAINT `fk_AnnonceCoLocation` FOREIGN KEY (`annonceCoLocation_id`) REFERENCES `AnnonceCoLocation`(`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
--- src/services/AnnonceCoLocationService.java
+++ src/services/AnnonceCoLocationService.java
@@ -5,19 +5,71 @@
  */
 package services;
 
+import interfaces.IAddressService;
 import java.util.List;
 import models.AnnonceCoLocation;
 import interfaces.IAnnonceCoLocationService;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import models.User;
+import technique.DataSource;
 
 /**
  *
  * @author kadhem
  */
-public class AnnonceCoLocationService implements IAnnonceCoLocationService{
+public class AnnonceCoLocationService implements IAnnonceCoLocationService {
+
+    private DataSource dataSource = DataSource.getInstance();
 
     @Override
     public void add(AnnonceCoLocation t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        try {
+            String req = "insert into AnnonceCoLocation(address_id,dimensions,maxCoLocataire, loyer,name,description,owner_id,creationDate,expirationDate) values(?,?,?,?,?,?,?,?,?)";
+            PreparedStatement ps = dataSource.getConnection().prepareStatement(req, PreparedStatement.RETURN_GENERATED_KEYS);
+
+            IAddressService addressService = new AddressService();
+            addressService.add(t.getAddress());
+
+            ps.setInt(1, t.getAddress().getId());
+            ps.setString(2, t.getDimensions());
+            ps.setInt(3, t.getMaxCoLocataire());
+            ps.setFloat(4, t.getLoyer());
+
+            ps.setString(5, t.getName());
+            ps.setString(6, t.getDescription());
+            ps.setInt(7, t.getOwner().getId());
+            ps.setDate(8, t.getCreationDate());
+            ps.setDate(9, t.getExpirationDate());
+
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            rs.next();
+
+            t.setId(rs.getInt((1)));
+            // adding the Images
+            String reqImageTable = "insert into ImageAnnonce(annonceCoLocation_id,imageUrl) values(?,?)";
+            PreparedStatement psImages = dataSource.getConnection().prepareStatement(reqImageTable);
+            for (String photo : t.getPhotosUrls()) {
+                psImages.setInt(1, t.getId());
+                psImages.setString(2,photo);
+                psImages.executeUpdate();
+            }
+            //adding "les Colocataires"
+            String reqCoLocatairesTable = "insert into AnnonceCoLocataire_R(annonceCoLocation_id,User_id) values(?,?)";
+            PreparedStatement psCoLocataires = dataSource.getConnection().prepareStatement(reqCoLocatairesTable);
+            for (User u : t.getCoLocataires()) {
+                psCoLocataires.setInt(1, t.getId());
+                psCoLocataires.setInt(2,u.getId());
+                psCoLocataires.executeUpdate();
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Error " + ex);
+        }
+
     }
 
     @Override
@@ -44,5 +96,5 @@ public class AnnonceCoLocationService implements IAnnonceCoLocationService{
     public AnnonceCoLocation search(AnnonceCoLocation t) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
