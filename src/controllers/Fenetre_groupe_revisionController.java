640c7896c201b1ed61a765d34c46e6ff2c86d52b
asus <asus@DESKTOP-27B7670>
2017-10-22 21:13:09 +0200

    update class  Revision constructeur

---
+++ src/controllers/Fenetre_groupe_revisionController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author asus
+ */
+public class Fenetre_groupe_revisionController implements Initializable {
+
+    @FXML
+    private AnchorPane topbar;
+    @FXML
+    private AnchorPane Description;
+    @FXML
+    private AnchorPane Chat;
+    @FXML
+    private AnchorPane compteRendu;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
