8ffbf1583291100f6f6f2bd0b3afe84bb466f566
firashamila33 <firas.hamila@esprit.tn>
2017-10-23 06:37:58 -0700

    food layouts

--- src/tests/Esprit4All.java
+++ src/tests/Esprit4All.java
@@ -6,39 +6,26 @@
 package tests;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
  *
- * @author Sami
+ * @author majdi
  */
 public class Esprit4All extends Application {
-    
+
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+         Parent root = FXMLLoader.load(getClass().getResource("/gui/FoodMainFXML.fxml"));
+       //Parent root = FXMLLoader.load(getClass().getResource("/gui/Club_EvenementFXML.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.setResizable(true);
+        stage.show();
     }
 
     /**
@@ -47,5 +34,5 @@ public class Esprit4All extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
