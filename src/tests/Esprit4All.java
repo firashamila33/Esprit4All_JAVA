d7bd841e94e6706d834d650459adf590d97d14bd
asus <asus@DESKTOP-27B7670>
2017-10-23 23:53:32 +0200

    update commit

--- src/tests/Esprit4All.java
+++ src/tests/Esprit4All.java
@@ -13,14 +13,16 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author danml
+ * @author majdi
  */
 public class Esprit4All extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
          //Parent root = FXMLLoader.load(getClass().getResource("/gui/AcceuilFXML.fxml"));
        Parent root = FXMLLoader.load(getClass().getResource("/gui/RevisionMainFXML.fxml"));
+
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
